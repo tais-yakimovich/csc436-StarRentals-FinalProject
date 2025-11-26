e007e34135671f7cfe06891e5c2a87b91cfefedb
MichaelM <63889044+MMartisiusCS@users.noreply.github.com>
2025-11-26 11:51:16 -0500

    added password to connection

--- StarRentalMng/database.py
+++ StarRentalMng/database.py
@@ -3,5 +3,5 @@ from databases import Database
 # Connection String
 # DATABASE_URL = "mysql+aiomysql://username:password@host:port/databaseName"
 
-DATABASE_URL = "mysql+aiomysql://root:@localhost:3306/Star_Rentals"
+DATABASE_URL = "mysql+aiomysql://root:password@localhost:3306/Star_Rentals"
 database = Database(DATABASE_URL)
